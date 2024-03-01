41570c7aa54600cd63c070492494926868c7af3a
“larissa.ribeiro” <larissa.ribeiro@g.globo>
2024-03-01 13:35:24 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Larissa Souza on 01/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
